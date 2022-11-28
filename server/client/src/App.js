ee2a8babfdb79f2211f31479a802faa82777017f
quentinberry <34132543+quentinberry@users.noreply.github.com>
2022-11-28 18:49:36 +0100

    more password functinality

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,32 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import './App.css';
+import PasswordList from './components/PasswordList';
 
 function App() {
-
-  const [backendData, setBackendData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/json")
-      .then(
-        res => res.json())
-      .then(
-        data => {
-          setBackendData(data)
-        });
-    console.log("fetch successful");
-  }, []);
-
-  console.log(backendData);
-
   return (
-    <div>
-      {
-        backendData.map(password => {
-          return(
-            <p>{password.Titel}</p>
-          )
-        })
-      }
-    </div>
+    <PasswordList/>
   )
 }
 
---
+++ server/client/src/components/PasswordList.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+function PasswordList() {
+
+    const [backendData, setBackendData] = useState([{}]);
+    const [visible, setVisible] = React.useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/json")
+      .then(
+        res => res.json())
+      .then(
+        data => {
+          setBackendData(data)
+        });
+  }, []);
+
+    return (
+        <div>
+          {
+            backendData.map(password => {
+              return(
+                <div className='entryShower'>
+                  <p>Password: {password.Titel}</p>
+                  <p>Username/Email: {password.Username}</p>
+                    <div className='passwordField'>
+                      <button onClick={() => setVisible(!visible)}> {visible ? '**********' : <p>{password.Password}</p>} </button>
+                    </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      )
+}
+
+export default PasswordList
